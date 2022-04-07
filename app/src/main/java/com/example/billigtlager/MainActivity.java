8b89a57c0e44372f9d47fb94fbc4ef7c01046331
Joachim Christiansen <Jussi196@hotmail.com>
2022-04-07 10:31:38 +0200

    Contact page

--- app/src/main/java/com/example/billigtlager/MainActivity.java
+++ app/src/main/java/com/example/billigtlager/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.contact_page);
 
         etTo = findViewById(R.id.ET_to);
         etSubject = findViewById(R.id.ET_subject);
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra(Intent.EXTRA_SUBJECT, etSubject.getText().toString());
             intent.putExtra(Intent.EXTRA_TEXT, etMessage.getText().toString());
             startActivity(intent);
+            Toast.makeText(this, "The Email was send", Toast.LENGTH_SHORT).show();;
         });
     }
 }
