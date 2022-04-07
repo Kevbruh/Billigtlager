add603254bc5074d464781a673ab39c7b075e01a
Joachim Christiansen <Jussi196@hotmail.com>
2022-04-07 11:49:18 +0200

    Contact page

--- app/src/main/java/com/example/billigtlager/MainActivity.java
+++ app/src/main/java/com/example/billigtlager/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etTo, etSubject, etMessage;
+    EditText etTo, etSubject, etMessage, etPhone, etName;
     Button btSend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +21,14 @@ public class MainActivity extends AppCompatActivity {
         etTo = findViewById(R.id.ET_to);
         etSubject = findViewById(R.id.ET_subject);
         etMessage = findViewById(R.id.ET_message);
+        etPhone = findViewById(R.id.ET_phone);
+        etName = findViewById(R.id.ET_name);
         btSend = findViewById(R.id.bt_send);
 
         btSend.setOnClickListener(view -> {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto: " + etTo.getText().toString()));
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"info@billigtlager.dk"});
             intent.putExtra(Intent.EXTRA_SUBJECT, etSubject.getText().toString());
             intent.putExtra(Intent.EXTRA_TEXT, etMessage.getText().toString());
             startActivity(intent);
