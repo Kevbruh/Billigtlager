0e2bd5b372a679370af77950931ab0f4fdbb25bc
Joachim Christiansen <Jussi196@hotmail.com>
2022-04-07 10:14:04 +0200

    Contact page

--- app/src/main/java/com/example/billigtlager/MainActivity.java
+++ app/src/main/java/com/example/billigtlager/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.billigtlager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etTo, etSubject, etMessage;
+    Button btSend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etTo = findViewById(R.id.ET_to);
+        etSubject = findViewById(R.id.ET_subject);
+        etMessage = findViewById(R.id.ET_message);
+        btSend = findViewById(R.id.bt_send);
+
+        btSend.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto: " + etTo.getText().toString()));
+            intent.putExtra(Intent.EXTRA_SUBJECT, etSubject.getText().toString());
+            intent.putExtra(Intent.EXTRA_TEXT, etMessage.getText().toString());
+            startActivity(intent);
+        });
     }
 }
