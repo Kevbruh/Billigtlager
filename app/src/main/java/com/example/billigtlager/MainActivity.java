ee8f706239b60197869cfb79bded8f6bf2751ff7
Joachim Christiansen <Jussi196@hotmail.com>
2022-05-11 09:50:28 +0200

    New room layouts

--- app/src/main/java/com/example/billigtlager/MainActivity.java
+++ app/src/main/java/com/example/billigtlager/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contact_page);
+        setContentView(R.layout.book_room);
 
         //EditText
         etTo = findViewById(R.id.ET_to);
